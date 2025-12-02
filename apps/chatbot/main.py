a2fcc8ffb8c4003a961d422c525ab375f40a02e3
cmtri2005 <tricaominh2005@gmail.com>
2025-12-02 07:29:37 +0000

    feat: add jd extract agent

--- apps/chatbot/main.py
+++ apps/chatbot/main.py
@@ -1,9 +1,13 @@
 from core.agents.resume_extract_agent import ResumeExtractAgent
+from core.agents.jd_extract_agent import JDExtractAgent
 from core.agents.orchestrator import Orchestrator
 
 
 if __name__ == "__main__":
     config = Orchestrator()
-    resume = ResumeExtractAgent("hihi",config.llm, config.tools)
+    resume = ResumeExtractAgent("hihi", config.llm, config.tools)
     result = resume.extract_resume("resume/Cao_Minh_Tri_Resume.pdf")
-    print(result)
+    print("-" * 20)
+    jd = JDExtractAgent("haha", config.llm, config.tools)
+    result_jd = jd.extract_jd("resume/url.docx")
+    print(result_jd)
